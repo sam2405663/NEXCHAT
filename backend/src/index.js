88387cec24963771e5cbd53ca3bb7577b51810dd
sam2405663 <samg58168@gmail.com>
2026-04-07 23:08:28 -0700

    save

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
